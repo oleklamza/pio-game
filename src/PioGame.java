10d42f376fbea31f503e055c764d394975df6213
olek_l <olek.lamza@gmail.com>
2019-10-29 13:42:22 +0100

    dodany step-1

---
+++ src/PioGame.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+/**
+ * Gra w odgadywanie wylosowanej liczby.
+ *
+ * Zasady:
+ * - komputer rzuca kostką (losuje liczby z zakresu 1..6)
+ * - gracz (też komputer) stara się odgadnąć liczbę (też losuje)
+ * - jeżeli odgadnie, gra się kończy
+ * - jeżeli nie odgadnie, rozpoczyna się kolejna runda (komputer losuje kolejną liczbę i gracz stara się ją odgadnąć)
+ */
+public class PioGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Random rand = new Random();     //obiekt losujący
+        int number;                     //wylosowana liczba
+        int guess;                      //propozycja (strzał) gracza
+
+        do {
+            System.out.println("---------------------");
+
+            number = rand.nextInt(6) + 1;
+            System.out.println("Kostka: " + number);
+
+            guess = rand.nextInt(6) + 1;
+            System.out.println("Gracz: " + guess);
+
+            if (number != guess) {
+                System.out.println("PUDŁO!");
+            }
+
+        } while (number != guess);
+
+        System.out.println("BRAWO!");
+    }
+
+}
